82d7cdb7842cfbc9c222ae8c1b2c240353541094
Jap3is <e4junks@yahoo.com>
2017-03-13 21:09:29 -0700

    Another trigger.

--- TicTacToe/tttMain.py
+++ TicTacToe/tttMain.py
@@ -6,8 +6,8 @@ def main():
     moves = [ "1", "2", "5", "9", "7", "4", "8", "3", "6" ]
     # moves = [ "1", "2", "3", "4", "5", "6", "8", "7", "9" ]
     ttt = TicTacToe()
-    ttt.Run( "o", moves )
-    # ttt.Run()
+    # ttt.Run( "o", moves )
+    ttt.Run()
         
 if __name__ == "__main__":
     main()
