b00ffbecaff978a3ea0afdc69aaffcbf825a318c
Jap3is <e4junks@yahoo.com>
2017-03-13 21:12:38 -0700

    Run moves3.

--- TicTacToe/tttMain.py
+++ TicTacToe/tttMain.py
@@ -3,11 +3,12 @@
 from ticTacToe import TicTacToe
 
 def main():
-    moves = [ "1", "2", "5", "9", "7", "4", "8", "3", "6" ]
-    # moves = [ "1", "2", "3", "4", "5", "6", "8", "7", "9" ]
+    moves1 = [ "1", "2", "5", "9", "7", "4", "8", "3", "6" ]
+    moves2 = [ "1", "2", "3", "4", "5", "6", "8", "7", "9" ]
+    moves3 = [ "1", "2", "3", "4", "5", "6", "7", "8", "9" ]
     ttt = TicTacToe()
-    # ttt.Run( "o", moves )
-    ttt.Run()
+    ttt.Run( "o", moves3 )
+    # ttt.Run()
         
 if __name__ == "__main__":
     main()
