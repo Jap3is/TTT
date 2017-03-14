4f992b1c5ca337a12f0ea4a967eeeae646e04a47
Jap3is <e4junks@yahoo.com>
2017-03-13 21:08:20 -0700

    main update to trigger jenkins.

--- TicTacToe/tttMain.py
+++ TicTacToe/tttMain.py
@@ -3,8 +3,8 @@
 from ticTacToe import TicTacToe
 
 def main():
-    # moves = [ "1", "2", "5", "9", "7", "4", "8", "3", "6" ]
-    moves = [ "1", "2", "3", "4", "5", "6", "8", "7", "9" ]
+    moves = [ "1", "2", "5", "9", "7", "4", "8", "3", "6" ]
+    # moves = [ "1", "2", "3", "4", "5", "6", "8", "7", "9" ]
     ttt = TicTacToe()
     ttt.Run( "o", moves )
     # ttt.Run()
