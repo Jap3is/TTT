3ff33123b93784d2f5b24a91057bb2916b8e63a4
Jap3is <e4junks@yahoo.com>
2017-03-13 20:33:12 -0700

    1st revision.

---
+++ TicTacToe/tttMain.py
@@ -0,0 +1,11 @@
+#!/Library/Frameworks/Python.framework/Versions/3.5/bin/python3
+
+from ticTacToe import TicTacToe
+
+def main():
+    ttt = TicTacToe()
+    ttt.SimulatedRun()
+        
+if __name__ == "__main__":
+    main()
+
